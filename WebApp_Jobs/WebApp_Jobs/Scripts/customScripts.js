37b0a6b691d612c29c362aed8e2f84fdafbc0bf1
rrolin <robinyeltsin@gmail.com>
2020-07-24 20:35:14 -0600

    finalMvcProject

---
+++ WebApp_Jobs/WebApp_Jobs/Scripts/customScripts.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#txtSearch").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#tblJobs tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
