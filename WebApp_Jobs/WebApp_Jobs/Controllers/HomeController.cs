b22e753312697c2b2db581a3c5e16c634eab7438
rrolin <robinyeltsin@gmail.com>
2020-07-24 18:11:34 -0600

    mvcProject

---
+++ WebApp_Jobs/WebApp_Jobs/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_Jobs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult About()
+        {
+            ViewBag.Message = "Test MVC5 Application.";
+
+            return View();
+        }
+    }
+}
